f71f2f410f089ca40a0d64bfd7725344ccf7a9c7
mrudani <mrudanisongade@gmail.com>
2024-01-17 19:15:35 +0530

    final commit

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.RoomMember)
+
+# superuser
+# name: mrudani
+# password: xyz@123
--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
+from accounts import views as av
 
 urlpatterns = [
-    path('',views.lobby),
-    path('room/',views.room)
+    path('',views.lobby,name='home'),
+    path('room/',views.room),
+    path('get_token/',views.getToken),
+    path('create_member/', views.createMember),
+    path('get_member/', views.getMember),
+    path('delete_member/', views.deleteMember),
+    path('login/',av.loginaccount,name='login'),
+    path('signupaccount/', av.signupaccount, name='signup'),
+    path('logout/', av.logoutaccount, name='logout'),
+    path("profile/", av.profile , name="profile"),
+    path('edit_details/=?<str:pk>/',av.edit_user,name='edit_details'),
 ]
